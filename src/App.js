08b8b4fea0892847892a35ab6c0f6a6c2a352837
yangwangzi <798530742@qq.com>
2020-09-23 13:28:26 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter,Switch,Route} from 'react-router-dom';
+import Login from './components/login';
+import Info from './components/info';
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    }
+  }
+  render (){
+    return (
+      <div>
+        <HashRouter>
+          <Switch>
+            <Route component={Login} exact path='/' />
+            <Route component={Login}  path='/login' />
+            <Route component={Info}  path='/info' />
+          </Switch>
+        </HashRouter>
+
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/info.js
@@ -0,0 +1,13 @@
+import React from 'react'
+class Info extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+    render(){
+        return (
+            <div>Info</div>
+        )
+    }
+}
+export default Info;
